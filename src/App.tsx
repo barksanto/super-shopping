1a5941131d6a8cff3dd898137d9405d94be77316
Barkley <barkley.santo@gmail.com>
2023-02-15 13:00:35 +0800

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import { Routes, Route } from "react-router-dom"
+import { Container } from "react-bootstrap"
+import { Home } from "./pages/Home"
+import { Store } from "./pages/Store"
+import { About } from "./pages/About"
+
+function App() {
+   return (
+     <Container>
+       <Routes>
+         {/* element corresponds to component to render */}
+         <Route path="/" element={<Home/>} />
+         <Route path="/store" element={<Store/>} />
+         <Route path="/about" element={<About/>} />
+       </Routes>
+     </Container>
+ )
+}
+
+export default App
